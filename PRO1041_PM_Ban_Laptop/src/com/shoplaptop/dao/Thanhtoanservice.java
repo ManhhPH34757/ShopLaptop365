26d1a2f79e318cc972ab6c6cf106c8753c60fb75
luongkhai1 <luongduykhai28082004@gmail.com>
2023-11-10 20:42:34 +0700

    update

---
+++ PRO1041_PM_Ban_Laptop/src/com/shoplaptop/dao/Thanhtoanservice.java
@@ -0,0 +1,74 @@
+package com.shoplaptop.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shoplaptop.entity.Hinhthucthanhtoan;
+import com.shoplaptop.utils.XJdbc;
+
+public class Thanhtoanservice implements ShopLaptop365DAO<Hinhthucthanhtoan, String>{
+
+	Connection connection = new XJdbc().Connect();
+	Statement st = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
+	
+	
+	ArrayList<Hinhthucthanhtoan> list = new ArrayList<Hinhthucthanhtoan>();
+	
+	@Override
+	public String insert(Hinhthucthanhtoan entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String update(Hinhthucthanhtoan entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Hinhthucthanhtoan selectById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Hinhthucthanhtoan> selectAll() {
+		// TODO Auto-generated method stub
+		try {
+			String sellectall = "SELECT * FROM dbo.HinhThucThanhToan";
+			st = connection.createStatement();
+			rs = st.executeQuery(sellectall);
+			while (rs.next()) {
+				int id = rs.getInt("ID");
+				String HinhThuc = rs.getString("HinhThuc");
+				
+				Hinhthucthanhtoan hinhthucthanhtoan = new Hinhthucthanhtoan(id, HinhThuc);
+				list.add(hinhthucthanhtoan);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			// TODO: handle exception
+		}
+		return list;
+	}
+
+	@Override
+	public List<Hinhthucthanhtoan> selectBySQL(String sql, Object... args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ PRO1041_PM_Ban_Laptop/src/com/shoplaptop/dao/Vanchuyenservice.java
@@ -0,0 +1,77 @@
+package com.shoplaptop.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shoplaptop.entity.Hinhthucvanchuyen;
+import com.shoplaptop.utils.XJdbc;
+
+public class Vanchuyenservice implements ShopLaptop365DAO<Hinhthucvanchuyen, String>{
+	
+	Connection connection = new XJdbc().Connect();
+	Statement st = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
+	
+	ArrayList<Hinhthucvanchuyen> list = new ArrayList<Hinhthucvanchuyen>();
+	
+	
+	
+	@Override
+	public String insert(Hinhthucvanchuyen entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String update(Hinhthucvanchuyen entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Hinhthucvanchuyen selectById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Hinhthucvanchuyen> selectAll() {
+		try {
+			String sellectall = "SELECT * FROM dbo.HinhThucVanChuyen";
+			st = connection.createStatement();
+			rs = st.executeQuery(sellectall);
+			while (rs.next()) {
+				int id = rs.getInt("ID");
+				String hinhthuc = rs.getString("HinhThuc");
+				String donvi = rs.getString("DonVi");
+				float giavc = rs.getFloat("GiaVC");
+				
+				Hinhthucvanchuyen hinhthucvanchuyen = new Hinhthucvanchuyen(id, hinhthuc, donvi, giavc);
+				list.add(hinhthucvanchuyen);
+				
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		// TODO Auto-generated method stub
+		return list;
+	}
+
+	@Override
+	public List<Hinhthucvanchuyen> selectBySQL(String sql, Object... args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
